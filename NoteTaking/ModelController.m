30f0df2d8d386bb7442f48bc3adc921bda5c23ee
Rui <rui@Students-MacBook-Pro.local>
2016-11-10 22:08:02 -0500

    implement flip forword

--- NoteTaking/ModelController.m
+++ NoteTaking/ModelController.m
@@ -72,7 +72,22 @@
 - (NSUInteger)indexOfViewController:(DataViewController *)viewController {
     // Return the index of the given data view controller.
     // For simplicity, this implementation uses a static array of model objects and the view controller stores the model object; you can therefore use the model object to identify the index.
-    return [self.pageData indexOfObject:viewController.dataObject];
+    
+    Person *classSpecifiedInstance = (Person *)viewController.dataObject;
+    if(classSpecifiedInstance != nil){
+        //Person Name
+        if(classSpecifiedInstance.name != nil){
+            NSLog(@"name = %@", classSpecifiedInstance.name);
+        }
+        if(classSpecifiedInstance.timestamp != nil){
+            NSLog(@"timestamp = %@", classSpecifiedInstance.timestamp);
+        }
+        if(classSpecifiedInstance.interaction != nil){
+            NSLog(@"interaction = %@", classSpecifiedInstance.interaction);
+        }
+    }
+    
+    return [self.pageData indexOfObject: viewController.dataObject];
 }
 
 
@@ -93,6 +108,7 @@
 {
     NSUInteger index = [self indexOfViewController:(DataViewController *)viewController];
     if (index == NSNotFound) {
+        
         return nil;
     }
     
@@ -107,8 +123,8 @@
     [self SaveCoreData:dataViewControllerInstance.nameTextField.text Date:dataViewControllerInstance.timestampTextField.text Interaction:dataViewControllerInstance.interactionTextView.text CardImage:uiImageData];
     
     index++;
+    
     if (index == [self.pageData count]) {
-        
         NSManagedObjectContext *managedObjectContext = [[DataManager sharedManager] managedObjectContext];
         
         NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Person" inManagedObjectContext:managedObjectContext];
@@ -116,14 +132,19 @@
         Person *person = [[Person alloc]initWithEntity: entityDescription insertIntoManagedObjectContext:managedObjectContext];
         
         person.name = @"";
-        person.timestamp = [self getDateString];
-        person.interaction = @"";
-        person.cardImage = nil;
+        dataViewControllerInstance.nameTextField.text=@"";
         
-        _pageData = [[NSMutableArray alloc] init];
+        NSString *timestamp =[self getDateString];
+        dataViewControllerInstance.timestampTextField.text=timestamp;
+        person.timestamp = timestamp;
+        
+        person.interaction = @"";
+        dataViewControllerInstance.interactionTextView.text=@"";
+        
+        person.cardImage = nil;
+        dataViewControllerInstance.cardImageView.image= nil;
         
         [_pageData addObject:person];
-        
         //return nil;
     }
     return [self viewControllerAtIndex:index storyboard:viewController.storyboard];
